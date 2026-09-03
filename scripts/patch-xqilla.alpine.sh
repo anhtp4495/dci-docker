fc7bc0f32ea9a419fdb9ae63b49fe73638baa1bf
atr <anh.tran@emotive.vn>
2026-09-03 16:07:14 +0700

    atr

---
+++ scripts/patch-xqilla.alpine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+FILE="/sources/XQilla-2.3.4/include/xqilla/ast/XQDocumentOrder.hpp"
+
+if grep -q 'bool operator()(const Node::Ptr &first, const Node::Ptr &second) const' "$FILE"; then
+    echo "XQilla comparator already patched"
+else
+    echo "Patching XQilla comparator"
+
+    sed -i \
+        's/bool operator()(const Node::Ptr \&first, const Node::Ptr \&second)$/bool operator()(const Node::Ptr \&first, const Node::Ptr \&second) const/' \
+        "$FILE"
+fi
